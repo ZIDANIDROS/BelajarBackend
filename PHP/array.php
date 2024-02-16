b1df49cc58e7220157300965b5ca13d1d2ec17d3
husein zidan <zidan.idros14@gmail.com>
2024-02-16 18:51:41 +0700

    latihan

--- PHP/array.php
+++ PHP/array.php
@@ -16,9 +16,30 @@ var_dump($names);
 var_dump(count($names));
 
 //Membuat Map di array php
-
+echo"-----------------Membuat Map di array php--------------------\n";
 $husein = array(
-    "Husein",
-    "Zidan",
-    30
+    "Name"=>"Husein",
+    "Name call"=>"Zidan",
+    "age"=>30
 );
+
+var_dump($husein);//memanggil semua
+
+var_dump($husein['Name call']);
+
+//Array didalam Array
+echo"\n-----------------array dalam array--------------------\n";
+$arrayDidalamArray = array(
+    "Name"=>"Husein",
+    "Name call"=>"Zidan",
+    "age"=>30,
+    "addres" => array(
+        "City"=> "Pasuruan",
+        "no" => 52,
+        "rw"=> 3,
+        "rt"=> 5
+    )
+);
+
+var_dump($arrayDidalamArray);
+var_dump($arrayDidalamArray["addres"]["City"]);
