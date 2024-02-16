3ec995bc79019b879e74951e15da6355a30507e0
husein zidan <zidan.idros14@gmail.com>
2024-02-16 17:54:05 +0700

    latihan

--- PHP/array.php
+++ PHP/array.php
@@ -6,3 +6,19 @@ var_dump($names[3]);
 
 $names[0] = "Tayo";
 var_dump($names);
+
+unset($names[1]);
+var_dump($names);
+
+$names[] = "joko";
+var_dump($names);
+
+var_dump(count($names));
+
+//Membuat Map di array php
+
+$husein = array(
+    "Husein",
+    "Zidan",
+    30
+);
