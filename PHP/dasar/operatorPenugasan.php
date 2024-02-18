4a719098f9ac6e69d4f0174d7db25afa3c882649
husein zidan <zidan.idros14@gmail.com>
2024-02-18 18:35:59 +0700

    latihan

--- PHP/dasar/operatorPenugasan.php
+++ PHP/dasar/operatorPenugasan.php
@@ -6,9 +6,9 @@ $fruit = 10000;
 $chicken = 35000;
 $orangeJuice = 1000;
 
-$total += $fruit;
-$total += $chicken;
-$total += $orangeJuice;
+$total += $fruit; //$total = $total + $fruit;
+$total += $chicken; //$total = $total + $chicken;
+$total += $orangeJuice; //$total = $total + $orangeJuice;
 
 var_dump($total);
 echo $total;
