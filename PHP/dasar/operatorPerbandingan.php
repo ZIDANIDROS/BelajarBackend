0a39aa6eb731e8ec91b7397c1e46f92cbcb19f10
husein zidan <zidan.idros14@gmail.com>
2024-02-18 18:43:45 +0700

    latihan

---
+++ PHP/dasar/operatorPerbandingan.php
@@ -0,0 +1,13 @@
+<?php
+
+//$a == $b ,sama dengan (menyamakan value)
+//$a === $b ,Identik
+//$a != $b ,tidak sama
+//$a <> $b ,lebih besar lebih kecil
+
+$a = "10";
+$b = 10;
+$c = $a == $b;
+
+var_dump($c); //true
+echo $c;
