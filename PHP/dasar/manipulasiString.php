fbb300293284a371e9c672687f14494856b9902f
husein zidan <zidan.idros14@gmail.com>
2024-02-19 12:46:26 +0700

    latihan manipulasi string

--- PHP/dasar/manipulasiString.php
+++ PHP/dasar/manipulasiString.php
@@ -3,4 +3,14 @@
 //dot operator
 $name = "Husein Zidan";
 echo "nama  : " . $name . PHP_EOL;
-echo "nilai : " . 100 . "\n";
+echo "nilai : " . 100 . "\n\n";
+
+//konfersi number ke string atau sebaliknya
+$valueString = (string)100;
+var_dump($valueString);
+
+$valueInt = (int)"100";
+var_dump($valueInt);
+
+$valueFloat = (float)"100.10";
+var_dump($valueFloat);
