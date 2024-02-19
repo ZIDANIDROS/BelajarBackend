b3d5de050f8715ba4f919aa1fae900be05d9fe7a
husein zidan <zidan.idros14@gmail.com>
2024-02-19 11:43:57 +0700

    latihan array opearator

--- PHP/dasar/operatorArray.php
+++ PHP/dasar/operatorArray.php
@@ -1,10 +1,11 @@
 <?php
-
+//menggabungkan 2 buah array
 $first = [
     "first name" => "eko"
 ];
 
 $second = [
+    "first name" => "eko", //di ignore karena sudah ada di first, berlaku jika di gabung
     "second name" => "tayo"
 ];
 
@@ -13,3 +14,17 @@ var_dump($full);
 foreach ($full as $fullz => $value) {
     echo $fullz . ": " . $value . "\n";
 }
+
+//perbandingan array
+$a = [
+    "first name" => "eko",
+    "second name" => "tayo"
+];
+
+$b = [
+    "first name" => "eko",
+    "second name" => "tayo"
+];
+
+var_dump($a == $b);
+echo $a == $b;
