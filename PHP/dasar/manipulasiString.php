42e3c0977c2dcd6a120066efee79dc470b01fdf2
husein zidan <zidan.idros14@gmail.com>
2024-02-19 12:50:06 +0700

    latihan manipulasi string

--- PHP/dasar/manipulasiString.php
+++ PHP/dasar/manipulasiString.php
@@ -14,3 +14,15 @@ var_dump($valueInt);
 
 $valueFloat = (float)"100.10";
 var_dump($valueFloat);
+echo "\n";
+
+//mengakses karakter
+$name = "HUSEIN";
+echo $name[0] . PHP_EOL;
+echo $name[1] . PHP_EOL;
+echo $name[2] . PHP_EOL;
+echo $name[3] . PHP_EOL;
+echo $name[4] . PHP_EOL;
+echo $name[5] . PHP_EOL;
+
+//variable Parsing
