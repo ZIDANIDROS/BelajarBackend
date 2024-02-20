d8e87addfd582a62829650954e464a8e86ef4828
husein zidan <zidan.idros14@gmail.com>
2024-02-20 17:33:10 +0700

    masih sama

--- PHP/dasar/forLoop.php
+++ PHP/dasar/forLoop.php
@@ -1,18 +1,15 @@
 <?php
 
 for ($i = 0; $i < 10; $i++) {
-    switch ($i):
-        case $i <= 10 / 2:
-            for ($j = 0; $j <= $i / 2; $j++) {
-                echo "*";
-            }
-            echo PHP_EOL;
-            break;
-        default:
-            for ($z =  $i - 1; $z >= 0; $z--) {
-                echo "*";
-            }
-            echo PHP_EOL;
-            break;
-    endswitch;
+    if ($i <= 10) {
+        for ($j = 0; $j <= $i / 2; $j++) {
+            echo "*";
+        }
+        echo PHP_EOL;
+    } else {
+        for ($z =  $i - 1; $z >= 0; $z--) {
+            echo "*";
+        }
+        echo PHP_EOL;
+    }
 }
