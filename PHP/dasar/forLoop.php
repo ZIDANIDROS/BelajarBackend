0f08889eccb001639fd7b438699d3141a6c27e7b
husein zidan <zidan.idros14@gmail.com>
2024-02-20 17:35:37 +0700

    latihan sama

--- PHP/dasar/forLoop.php
+++ PHP/dasar/forLoop.php
@@ -1,15 +1,19 @@
 <?php
 
 for ($i = 0; $i < 10; $i++) {
-    if ($i <= 10) {
+    if ($i < 5) {
         for ($j = 0; $j <= $i / 2; $j++) {
             echo "*";
         }
         echo PHP_EOL;
-    } else {
+    }
+    if ($i > 5) {
         for ($z =  $i - 1; $z >= 0; $z--) {
             echo "*";
         }
         echo PHP_EOL;
+    } else {
+        echo PHP_EOL;
+        echo "FINISH";
     }
 }
