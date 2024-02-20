83b6fa95e86a32642a069701ff11fd2c162c4c44
husein zidan <zidan.idros14@gmail.com>
2024-02-20 17:51:45 +0700

    selesai while dan do whileLoop

--- PHP/dasar/whileLoop.php
+++ PHP/dasar/whileLoop.php
@@ -3,6 +3,13 @@
 $counter = 1;
 
 while ($counter <= 10) {
-    echo "Hello world : $counter" . PHP_EOL;
+    echo "Hello world : $counter\n";
     $counter++;
 }
+
+echo "\n";
+$counter2 = 1;
+do {
+    echo "Hello world : $counter2\n";
+    $counter2++;
+} while ($counter2 <= 10);
