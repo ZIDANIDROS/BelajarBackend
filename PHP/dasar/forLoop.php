03f44d0bc117f249d5647139b019cc96795019b6
husein zidan <zidan.idros14@gmail.com>
2024-02-20 17:42:04 +0700

    hampir selsei

--- PHP/dasar/forLoop.php
+++ PHP/dasar/forLoop.php
@@ -1,19 +1,15 @@
 <?php
 
-for ($i = 0; $i < 10; $i++) {
-    if ($i < 5) {
-        for ($j = 0; $j <= $i / 2; $j++) {
-            echo "*";
-        }
-        echo PHP_EOL;
-    }
-    if ($i > 5) {
-        for ($z =  $i - 1; $z >= 0; $z--) {
-            echo "*";
+for ($i = 0; $i < 6; $i++) {
+    if ($i <= 2) {
+        for ($j = 0; $j <= $i; $j++) {
+            echo "+";
         }
         echo PHP_EOL;
     } else {
+        for ($z =  5; $z >= 0; $z--) {
+            echo "-";
+        }
         echo PHP_EOL;
-        echo "FINISH";
     }
 }
