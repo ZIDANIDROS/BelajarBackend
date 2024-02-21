cddf6df676f65b396a2f6486231800546de782d8
husein zidan <zidan.idros14@gmail.com>
2024-02-21 08:33:10 +0700

    latihan forEach

--- PHP/dasar/forEach.php
+++ PHP/dasar/forEach.php
@@ -5,7 +5,27 @@ $names = ["Husein", "Zidan", "Alaydrus"];
 for ($i = 0; $i < count($names); $i++) {
     echo "Hello mas $names[$i]" . PHP_EOL;
 }
+echo "\n";
 
+//foreach tidak butuh index
 foreach ($names as $name) {
     echo "Hello $name" . PHP_EOL;
 }
+echo "\n";
+
+$person = [
+    "First name" => "Husein",
+    "middle name" => "Zidan",
+    "last name" => "alaydrus"
+];
+
+//foreach tidak butuh index
+foreach ($person as $key => $lawak) {
+    echo "$key : $lawak" . PHP_EOL;
+}
+echo "\n";
+
+foreach ($names as $index => $name) {
+    echo "Hello $name index ke-$index" . PHP_EOL;
+}
+echo "\n";
