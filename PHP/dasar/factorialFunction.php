1de711bd1119569158f96febcb817b250fbcad00
husein zidan <zidan.idros14@gmail.com>
2024-02-22 20:00:34 +0700

    Latihan faktorial tanpa nyontek yg rekrusif nyontek

--- PHP/dasar/factorialFunction.php
+++ PHP/dasar/factorialFunction.php
@@ -9,4 +9,14 @@ function factorial(int $a)
     return $result;
 }
 
-echo factorial(5);
+var_dump(factorial(5));
+
+function rekrusif(int $result)
+{
+    if ($result == 1) {
+        return 1;
+    } else {
+        return  $result * rekrusif($result - 1);
+    }
+}
+var_dump(rekrusif(5));
