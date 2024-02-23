6e861f11178c5e5264eed0c420b272eef96d8f4d
husein zidan <zidan.idros14@gmail.com>
2024-02-24 06:19:51 +0700

    latihan arrayFunction dan stringFunction

---
+++ PHP/dasar/stringFunction.php
@@ -0,0 +1,8 @@
+<?php
+
+var_dump(join(",", [1, 2, 3, 4, 5, 6, 7, 8, 9])); //"," adalah pemecah untuk menjadi array
+var_dump(explode(" ", "Husein Zidan Alaydrus"));
+var_dump(strtolower("Husein Zidan Alaydrus"));
+var_dump(strtoupper("Husein Zidan Alaydrus"));
+var_dump(substr("Husein Zidan Alaydrus", 0, 21));
+var_dump(trim("               Husein ZIDAN                  "));
