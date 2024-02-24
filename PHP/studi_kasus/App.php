3c4ac411030c462d959849aafc8ae67ae1c75faf
husein zidan <zidan.idros14@gmail.com>
2024-02-24 09:47:40 +0700

    mengatur foreach

--- PHP/studi_kasus/App.php
+++ PHP/studi_kasus/App.php
@@ -1,9 +1,9 @@
 <?php
 
 $todolist = [
-    0 => "Membaca",
-    1 => "membuat course",
-    2 => "makan siang"
+    1 => "Membaca",
+    2 => "membuat course",
+    3 => "makan siang"
 ];
 
 $memilih = [
@@ -17,7 +17,7 @@ do {
     echo "TODO LIST" . PHP_EOL;
     echo "list harian" . PHP_EOL;
     foreach ($todolist as $key => $lawak) {
-        echo $key + 1 . "." . $lawak . PHP_EOL;
+        echo "$key. $lawak" . PHP_EOL;
     }
     echo "\n\n";
     foreach ($memilih as $key) {
@@ -49,7 +49,7 @@ do {
 
     echo "\n\n";
     foreach ($todolist as $key => $lawak) {
-        echo $key + 1 . "." . $lawak . PHP_EOL;
+        echo "$key. $lawak" . PHP_EOL;
     }
     echo "apakah ingin kembali ke menu ?(y/n) ";
     $boolMENU = readline();
