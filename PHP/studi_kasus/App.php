c612e9afeedf87d1758e3c1e0c8046e0a60d93dd
husein zidan <zidan.idros14@gmail.com>
2024-02-24 07:17:44 +0700

    studi_kasus todolist

--- PHP/studi_kasus/App.php
+++ PHP/studi_kasus/App.php
@@ -1,1 +1,18 @@
 <?php
+
+$todolist = [
+    1 => "Membaca",
+    2 => "membuat course",
+    3 => "makan siang"
+];
+
+foreach ($todolist as $key => $lawak) {
+    echo "$key. $lawak" . PHP_EOL;
+}
+
+$inputan = readline();
+
+if ($inputan == 1) {
+} else {
+    # code...
+}
