7cbd2c73c8a30ad7744e49b3f393927113dc329c
husein zidan <zidan.idros14@gmail.com>
2024-02-24 12:03:13 +0700

    oproses pembenahan logic delet

--- PHP/studi_kasus/App.php
+++ PHP/studi_kasus/App.php
@@ -1,9 +1,7 @@
 <?php
 
 $todolist = [
-    "Membaca",
-    "membuat course",
-    "makan siang"
+    "Membaca", "membuat course", "makan siang"
 ];
 
 $memilih = [
@@ -17,17 +15,16 @@ do {
     echo "\033[2J\033[;H";
     echo "TODO LIST" . PHP_EOL;
     echo "list harian" . PHP_EOL;
-    for ($i = 0; $i <= count($todolist); $i++) {
-        echo $i + 1 . ". " . $todolist[$i] . PHP_EOL;
+    for ($i = 0; $i < count($todolist); $i++) {
+        echo ($i + 1) . ". " . $todolist[$i] . PHP_EOL;
     }
     echo "\n\n";
-    foreach ($memilih as $key) {
-        echo $key . PHP_EOL;
+    foreach ($memilih as $key => $value) {
+        echo "$key. $value" . PHP_EOL;
     }
 
     echo "Masukan pilihan : ";
     $inputan = readline();
-    $memilih[] = $inputan;
 
     if ($inputan == 1) {
         do {
@@ -39,18 +36,29 @@ do {
     } elseif ($inputan == 2) {
         do {
             echo "Masukan nomor yang ingin dihapus : ";
-            $index = (int) readline();
-            unset($todolist[$index]);
+            $CMD = (int) readline();
+            $index = $CMD - 1;
+            if (isset($todolist[$index])) {
+                unset($todolist[$index]);
+                for ($i = 0; $i < $todolist; $i++) {
+                    if ($todolist[$i] == null) {
+                        $todolist[$i] = $todolist[$i + 1];
+                    }
+                }
+            } else {
+                echo "Nomor indeks tidak valid!" . PHP_EOL;
+            }
             echo "apakah ingin menghapus lagi ?(y/n) ";
             $boolIF = readline();
         } while ($boolIF == 'y' || $boolIF == 'Y');
     } else {
+        echo "Terima kasih telah menggunakan program. Sampai jumpa!";
         break;
     }
 
     echo "\n\n";
-    for ($i = 0; $i <= count($todolist); $i++) {
-        echo $i + 1 . ". " . $todolist[$i] . PHP_EOL;
+    for ($i = 0; $i < count($todolist); $i++) {
+        echo $i + 1  . ". " . $todolist[$i] . PHP_EOL;
     }
     echo "apakah ingin kembali ke menu ?(y/n) ";
     $boolMENU = readline();
