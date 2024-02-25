45dd226364056fef82f1abce1a88e715f16fa717
husein zidan <zidan.idros14@gmail.com>
2024-02-25 07:26:14 +0700

    function di Person.php

--- PHP/OOP/data/Person.php
+++ PHP/OOP/data/Person.php
@@ -5,4 +5,9 @@ class Person
     var string $name;
     var ?string $gender = null; //nulable = bisa/noleh null
     var string $country;
+
+    function sayHello(string $name)
+    {
+        echo "Hello $name" . PHP_EOL;
+    }
 }
