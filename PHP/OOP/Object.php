3868045fbb79148c1228e4410a3e5e12a586af96
husein zidan <zidan.idros14@gmail.com>
2024-02-25 07:37:36 +0700

    this keyword

--- PHP/OOP/Object.php
+++ PHP/OOP/Object.php
@@ -6,15 +6,14 @@ $person = new Person;
 $person->name = "Husein Zidan";
 $person->gender = "male";
 $person->country = "Indonesia";
-
+$person->sayHello("Husein");
 var_dump($person);
+echo "\n\n";
 
-echo "Name    : {$person->name}" . PHP_EOL;
-echo "gender  : {$person->gender}" . PHP_EOL;
-echo "country : {$person->country}" . PHP_EOL;
 
 $person2 = new Person;
-$person2->name = "Adi";
+$person2->name = "Moroko";
 $person2->gender = "male";
 $person2->country = "Indonesia";
+$person2->sayHello(null);
 var_dump($person2);
--- PHP/OOP/data/Person.php
+++ PHP/OOP/data/Person.php
@@ -3,11 +3,15 @@
 class Person
 {
     var string $name;
-    var ?string $gender = null; //nulable = bisa/noleh null
+    var ?string $gender; //nulable = bisa/noleh null
     var string $country;
 
-    function sayHello(string $name)
+    function sayHello(?string $name)
     {
-        echo "Hello $name" . PHP_EOL;
+        if (is_null($name)) {
+            echo "Hi my name is {$this->name}" . PHP_EOL;
+        } else {
+            echo "Hi my name is $name" . PHP_EOL;
+        }
     }
 }
