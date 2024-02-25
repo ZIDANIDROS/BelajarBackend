3b59d1a20b47e6a69eb601f3512b32e20576e79f
husein zidan <zidan.idros14@gmail.com>
2024-02-25 22:47:01 +0700

    ImportAlias ke-2

--- PHP/OOP/ImportAlias.php
+++ PHP/OOP/ImportAlias.php
@@ -5,11 +5,11 @@ require_once "data/conflict.php";
 use data\one\Conflict as Conflict1;
 use data\two\Conflict as Conflict2;
 use function Helper\helpMe as help;
-use const Helper\APPLICATION as app;
+use const Helper\APPLICATION as APP;
 
 $conflict1 = new Conflict1();
 $conflict2 = new Conflict2();
 
 help();
 
-echo app . PHP_EOL;
+echo APP . PHP_EOL;
