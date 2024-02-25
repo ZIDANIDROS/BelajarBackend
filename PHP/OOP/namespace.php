df6f810977d7abb0c6dfa72aa1348bf2f99ab44d
husein zidan <zidan.idros14@gmail.com>
2024-02-25 16:58:34 +0700

    namespace, dan membuat object di namespace

---
+++ PHP/OOP/namespace.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once "data/conflict.php";
+require_once "data/Helper.php";
+
+$conflict1 = new data\one\Conflict();
+$conflict2 = new data\two\Conflict();
+
+echo Helper\APPLICATION . PHP_EOL;
+Helper\helpMe();
