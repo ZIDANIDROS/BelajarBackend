590d90337fcb9b53089890b47fcf540a04e35377
husein zidan <zidan.idros14@gmail.com>
2024-02-26 10:44:04 +0700

    Parent

---
+++ PHP/OOP/data/superKeyword.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Data;
+
+class Shape
+{
+    function getCorner(): int
+    {
+        return 0;
+    }
+}
+
+class Rectangel extends shape
+{
+    public function getCorner(): int
+    {
+        return 4;
+    }
+
+    public function getParentCorner(): int
+    {
+        return parent::getCorner();
+    }
+}
