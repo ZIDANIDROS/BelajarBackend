a294e73ff45c004e3847075e65a839a1b12e3a71
husein zidan <zidan.idros14@gmail.com>
2024-02-26 15:00:07 +0700

    memanggil Polymorphism di Polymorphism.php

--- PHP/OOP/Polymorphism.php
+++ PHP/OOP/Polymorphism.php
@@ -4,3 +4,10 @@ require_once "data/Programmer.php";
 
 $company = new Company();
 $company->programmer = new Programmer("Husein");
+var_dump($company);
+
+$company->programmer = new backendProgrammer("Husein");
+var_dump($company);
+
+$company->programmer = new frontendProgrammer("Husein");
+var_dump($company);
