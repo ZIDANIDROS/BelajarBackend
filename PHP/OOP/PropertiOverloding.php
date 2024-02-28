8167477cae113573bfe248005ff04d7f158dc46c
husein zidan <zidan.idros14@gmail.com>
2024-02-28 22:44:40 +0700

    PropertiOverloding

---
+++ PHP/OOP/PropertiOverloding.php
@@ -0,0 +1,35 @@
+<?php
+
+class Zero
+{
+    private array $properties = [];
+
+    public function __get($name)
+    {
+        return $this->properties[$name];
+    }
+
+    public function __set($name, $value)
+    {
+        $this->properties[$name] = $value;
+    }
+
+    public function __isset($name): bool
+    {
+        return isset($this->properties[$name]);
+    }
+
+    public function __unset($name)
+    {
+        unset($this->properties[$name]);
+    }
+}
+
+$zero = new Zero();
+$zero->firstName = "Husein";
+$zero->middleName = "Zidan";
+$zero->lastName = "Alaydrus";
+
+echo "First Name : $zero->firstName" . PHP_EOL;
+echo "middle Name : $zero->middleName" . PHP_EOL;
+echo "last Name : $zero->lastName" . PHP_EOL;
