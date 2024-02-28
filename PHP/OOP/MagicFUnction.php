b1c7021c1170d240a88e381381e989b0d04b343a
husein zidan <zidan.idros14@gmail.com>
2024-02-28 21:35:08 +0700

    magicalFunction "__debugInfo"

--- PHP/OOP/MagicFUnction.php
+++ PHP/OOP/MagicFUnction.php
@@ -7,7 +7,12 @@ $student1->id = "1";
 $student1->name = "Husein";
 $student1->value = 100;
 
+//magicalFunction "__toString"
 $string = (string) $student1;
 echo $string . PHP_EOL;
 
+//magicalFunction "__invoke"
 $student1(1, "Husein", true, "Manusia");
+
+//magicalFunction "__debugInfo"
+var_dump($student1);
--- PHP/OOP/data/Student.php
+++ PHP/OOP/data/Student.php
@@ -18,4 +18,15 @@ class Student
         $join = join("\n", $arguments);
         echo "Invoice Student with arguments $join" . PHP_EOL;
     }
+
+    //magicalFunction "__debugInfo"
+    public function __debugInfo(): array
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "value" => $this->value,
+            "author" => "Husein Zidan Alaydrus"
+        ];
+    }
 }
