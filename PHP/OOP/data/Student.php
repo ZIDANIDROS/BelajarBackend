096f358827c2c30d50ba6d9fb01f5dd2a1200c76
husein zidan <zidan.idros14@gmail.com>
2024-02-28 12:52:04 +0700

    MagicFUnction __toString

--- PHP/OOP/data/Student.php
+++ PHP/OOP/data/Student.php
@@ -5,4 +5,15 @@ class Student
     public string $id;
     public string $name;
     public int $value;
+
+    //magicalFunction "__toString"
+    public function __toString(): string
+    {
+        return "Student id : $this->id, name : $this->name, Value : $this->value";
+    }
+
+    //magicalFunction "__invoke"
+    public function __invoke(...$arguments){
+        $join = join(",")
+    }
 }
