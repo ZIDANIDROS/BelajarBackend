1412e702ebe7a6fe778ebfe6b6093a7728317f5a
husein zidan <zidan.idros14@gmail.com>
2024-02-29 09:01:17 +0700

    DateTime

--- PHP/OOP/DateTime.php
+++ PHP/OOP/DateTime.php
@@ -2,4 +2,13 @@
 
 $dateTime = new DateTime();
 $dateTime->setDate(1999, 9, 11);
+$dateTime->setTime(12, 12, 12);
+
+$dateTime->add(new DateInterval("P1Y"));
+
+$dateInterval = new DateInterval("P1M");
+$dateInterval->invert = 1;
+
+$dateTime->add($dateInterval);
+
 var_dump($dateTime);
