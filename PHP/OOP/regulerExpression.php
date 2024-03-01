d8a4dff7e697e63a5881743c2453a010bb238a82
husein zidan <zidan.idros14@gmail.com>
2024-03-01 08:12:34 +0700

    regulerExpression preg_replace

--- PHP/OOP/regulerExpression.php
+++ PHP/OOP/regulerExpression.php
@@ -2,6 +2,9 @@
 
 $matches = [];
 $result = (bool)preg_match_all("/husein | zidan | edy/i", "Husein Zindan Alaydrus", $matches);
-
 var_dump($result);
 var_dump($matches);
+
+//menangani kata-kata kasar
+$result = preg_replace("/anjing|bangsat/i", "***", "dasar lu anak bangsat");
+var_export($result);
