6174bb4f406dbf39dbc48856cb31a47bc2aebf6b
husein zidan <zidan.idros14@gmail.com>
2024-03-01 10:54:15 +0700

    Validation Reflection

--- PHP/OOP/Reflection.php
+++ PHP/OOP/Reflection.php
@@ -5,5 +5,22 @@ require_once "data/loginRequest.php";
 require_once "helper/ValidationUtil.php";
 
 $request = new LoginRequest();
+$request->username = null;
+$request->password = null;
 
-ValidationUtil::validate($request);
+// ValidationUtil::validate($request);
+ValidationUtil::validateReflection($request);
+
+class RegisterUserRequest
+{
+    public ?string $name;
+    public ?string $address;
+    public ?string $email;
+}
+
+$register = new RegisterUserRequest();
+$register->name = "Husein";
+$register->address = "zidan";
+$register->email = "zidan@gmail.com";
+
+ValidationUtil::validateReflection($register);
