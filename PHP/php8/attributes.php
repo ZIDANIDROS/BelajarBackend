c47e214f2793f5661ba56c5e60b035e394a95cfc
husein zidan <zidan.idros14@gmail.com>
2024-03-01 22:51:19 +0700

    membaca attribute via reflection

--- PHP/php8/attributes.php
+++ PHP/php8/attributes.php
@@ -11,3 +11,12 @@ class LoginRequest
     public string $username;
     public string $password;
 }
+
+function validate(object $object): void
+{
+    $class = new ReflectionClass($object);
+    $properties = $class->getProperties();
+    foreach ($properties as $property) {
+        validateNotBlank($property, $object);
+    }
+}
