ccc5ae7cb9ff0df773da3ba3b171a0e4f1f16d33
husein zidan <zidan.idros14@gmail.com>
2024-03-03 20:38:18 +0700

    Constructor cara baru tanpa this->...

---
+++ PHP/php8/ConstructorPromotion.php
@@ -0,0 +1,24 @@
+<?php
+//cara baru
+class product
+{
+    public function __construct(public string $id, public string $name, public int $price, public int $quantity)
+    {
+    }
+}
+// cara lama
+// class Product
+// {
+//     public string $id;
+//     public string $name;
+//     public int $price;
+//     public int $quantity;
+
+//     public function __construct(string $id, string $name, int $price, int $quantity)
+//     {
+//         $this->id = $id;
+//         $this->name = $name;
+//         $this->price = $price;
+//         $this->quantity = $quantity;
+//     }
+// }
