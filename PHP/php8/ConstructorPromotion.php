6eb7371af38222328c256cfb1e6862baf48ed94c
husein zidan <zidan.idros14@gmail.com>
2024-03-03 20:45:48 +0700

    proses hasil

--- PHP/php8/ConstructorPromotion.php
+++ PHP/php8/ConstructorPromotion.php
@@ -1,24 +1,28 @@
 <?php
-//cara baru
-class product
+class Product
 {
-    public function __construct(public string $id, public string $name, public int $price, public int $quantity)
+    public function __construct(public string $id, public string $name, public int $price, public int $quantity = 0)
     {
     }
 }
+
+$product = new Product("1", "AHAHA", 15000);
+
+
+
 // cara lama
 // class Product
 // {
-//     public string $id;
-//     public string $name;
-//     public int $price;
-//     public int $quantity;
+// public string $id;
+// public string $name;
+// public int $price;
+// public int $quantity;
 
-//     public function __construct(string $id, string $name, int $price, int $quantity)
-//     {
-//         $this->id = $id;
-//         $this->name = $name;
-//         $this->price = $price;
-//         $this->quantity = $quantity;
-//     }
+// public function __construct(string $id, string $name, int $price, int $quantity)
+// {
+// $this->id = $id;
+// $this->name = $name;
+// $this->price = $price;
+// $this->quantity = $quantity;
+// }
 // }
