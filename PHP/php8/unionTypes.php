294ac1e5460bd9296245350682f3e95f67e4eae8
husein zidan <zidan.idros14@gmail.com>
2024-03-03 21:17:22 +0700

    unionTypes return value

--- PHP/php8/unionTypes.php
+++ PHP/php8/unionTypes.php
@@ -20,5 +20,16 @@ function sampleFunction(string|array $data): void
         echo "Argument is array" . PHP_EOL;
     }
 }
-
 sampleFunction("Husein");
+
+function sampleFunction2(string|array $data): string|array
+{
+    if (is_string($data)) {
+        return "string" . PHP_EOL;
+    }
+    if (is_array($data)) {
+        return ["array"] . PHP_EOL;
+    }
+}
+
+echo sampleFunction2("Husein");
