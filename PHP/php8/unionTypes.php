54b6a5967859b4aaef528b53bec1d603a298aa37
husein zidan <zidan.idros14@gmail.com>
2024-03-03 21:13:52 +0700

    unionTypes argument

--- PHP/php8/unionTypes.php
+++ PHP/php8/unionTypes.php
@@ -10,3 +10,15 @@ $example->data = 12;
 $example->data = "Husein Zidan";
 $example->data = true;
 $example->data = [];
+
+function sampleFunction(string|array $data): void
+{
+    if (is_string($data)) {
+        echo "Argument is string" . PHP_EOL;
+    }
+    if (is_array($data)) {
+        echo "Argument is array" . PHP_EOL;
+    }
+}
+
+sampleFunction("Husein");
