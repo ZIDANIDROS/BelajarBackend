ea230fb6825e1404b231807b63420f3f3b920257
husein zidan <zidan.idros14@gmail.com>
2024-03-04 06:03:46 +0700

    Nullsafe operator

--- PHP/php8/NullSafeOperator.php
+++ PHP/php8/NullSafeOperator.php
@@ -20,4 +20,10 @@ function getCountry(?User $user): ?string
     }
     return null;
 }
+
+//Nullsafe operator di php 8
+function getCountry2(?User $user): ?string
+{
+    return $user?->address?->country;
+}
 echo getCountry(null) . PHP_EOL;
