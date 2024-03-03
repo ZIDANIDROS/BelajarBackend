a2bcebeb39e9c4a66593b074666d71a171b727bf
husein zidan <zidan.idros14@gmail.com>
2024-03-03 20:46:29 +0700

    supaya 20

--- PHP/php8/ConstructorPromotion.php
+++ PHP/php8/ConstructorPromotion.php
@@ -9,6 +9,17 @@ class Product
 $product = new Product("1", "AHAHA", 15000);
 
 
+
+// cara lama
+// class Product
+// {
+// public string $id;
+// public string $name;
+// public int $price;
+// public int $quantity;
+
+// public function __construct(string $id, string $name, int $price, int $quantity)
+// {
 // $this->id = $id;
 // $this->name = $name;
 // $this->price = $price;
