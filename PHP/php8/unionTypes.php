66b246ac961e5267110ac9309aa73f1cf3978272
husein zidan <zidan.idros14@gmail.com>
2024-03-03 21:33:23 +0700

    merapihin

--- PHP/php8/unionTypes.php
+++ PHP/php8/unionTypes.php
@@ -22,14 +22,13 @@ function sampleFunction(string|array $data): void
 }
 sampleFunction("Husein");
 
-function sampleFunction2(string|array $data): string|array
-{
-    if (is_string($data)) {
-        return "string" . PHP_EOL;
-    }
-    if (is_array($data)) {
-        return ["array"] . PHP_EOL;
-    }
-}
-
-echo sampleFunction2("Husein");
+// function sampleFunction2(string|array $data): string|array
+// {
+//     if (is_string($data)) {
+//         return "string" . PHP_EOL;
+//     }
+//     if (is_array($data)) {
+//         return ["array"] . PHP_EOL;
+//     }
+//}
+// echo sampleFunction2("Husein");
