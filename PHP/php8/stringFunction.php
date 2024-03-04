3d144a2fb8f6e8a6a8ad5f59f12b546534a963f4
husein zidan <zidan.idros14@gmail.com>
2024-03-04 22:29:20 +0700

    stringFunction

---
+++ PHP/php8/stringFunction.php
@@ -0,0 +1,10 @@
+<?php
+
+var_dump(str_contains("Husein", "Zidan"));
+var_dump(str_contains("Husein Zidan", "Zidan"));
+
+var_dump(str_starts_with("Husein", "Zidan"));
+var_dump(str_starts_with("Husein Zidan", "Zidan"));
+
+var_dump(str_ends_with("Husein", "Zidan"));
+var_dump(str_ends_with("Husein Zidan", "Zidan"));
