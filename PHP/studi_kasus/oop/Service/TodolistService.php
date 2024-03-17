b262ed0ad356124dc102f3cc67ecdd9a737d107d
husein zidan <zidan.idros14@gmail.com>
2024-03-18 01:53:05 +0700

    pembenahan show Todolist

--- PHP/studi_kasus/oop/Service/TodolistService.php
+++ PHP/studi_kasus/oop/Service/TodolistService.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Service {
+    use Repository\TodolistRepository;
     interface TodolistService
     {
         function showTodolist(): void;
@@ -10,8 +11,19 @@ namespace Service {
 
     class TodolistRepositoryImpl implements TodolistService
     {
+        private TodolistRepository $todolistRepository;
+
+        public function __cunstruct(TodolistRepository $todolistRepository){
+            $this->todolistRepository = $todolistRepository;
+        }
+        private TodolistRepository
         function showTodolist(): void
         {
+            echo "TODOLIST" . PHP_EOL;
+            $todolist = $this->todolistRepository->findAll();
+            foreach ($this->todolistRepository->findAll() as $number => $value) {
+                echo "$number . $value" . PHP_EOL;
+            }
         }
         function addTodolist(string $todo): void
         {
