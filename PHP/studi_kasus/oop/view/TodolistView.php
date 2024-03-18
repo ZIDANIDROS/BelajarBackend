3bb598c50cf5f171e762a6f33b3ebed60fa9f67b
husein zidan <zidan.idros14@gmail.com>
2024-03-18 17:35:39 +0700

    membuat inputan

--- PHP/studi_kasus/oop/view/TodolistView.php
+++ PHP/studi_kasus/oop/view/TodolistView.php
@@ -2,10 +2,28 @@
 
 namespace View {
 
+    use Service\TodolistService;
+
     class TodolistView
     {
+        private TodolistService $todolistService;
+
+        public function __construct(TodolistService $todolistService)
+        {
+            $this->todolistService = $todolistService;
+        }
         function showTodolist(): void
         {
+            while (true) {
+                $this->todolistService->showTodolist();
+
+                echo "Menu" . PHP_EOL;
+                echo "1. Tambah Todo" . PHP_EOL;
+                echo "2. Tambah Todo" . PHP_EOL;
+                echo "3. Tambah Todo" . PHP_EOL;
+
+                $pilihan = input("pilih");
+            }
         }
         function addTodolist(): void
         {
