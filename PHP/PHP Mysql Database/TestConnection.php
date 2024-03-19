0113ed1f99c74d4ec78c00d7222d60da0d0eb962
husein zidan <zidan.idros14@gmail.com>
2024-03-19 20:54:19 +0700

    merapihin

--- PHP/PHP Mysql Database/TestConnection.php
+++ PHP/PHP Mysql Database/TestConnection.php
@@ -6,6 +6,6 @@ $database = "belajar_php_database";
 $username = "root";
 $password = "";
 
-    $connection = new PDO("mysql:host=$host;port=$port;dbname=$database", $username, $password);
-    echo "Sukses terkoneksi ke database";
+$connection = new PDO("mysql:host=$host;port=$port;dbname=$database", $username, $password);
+echo "Sukses terkoneksi ke database";
 
