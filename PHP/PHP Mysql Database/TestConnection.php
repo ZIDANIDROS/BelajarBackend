a820dee28d0303ddce21b62596d063e8416a00c4
husein zidan <zidan.idros14@gmail.com>
2024-03-19 20:54:07 +0700

    cob2

--- PHP/PHP Mysql Database/TestConnection.php
+++ PHP/PHP Mysql Database/TestConnection.php
@@ -6,9 +6,6 @@ $database = "belajar_php_database";
 $username = "root";
 $password = "";
 
-try {
     $connection = new PDO("mysql:host=$host;port=$port;dbname=$database", $username, $password);
     echo "Sukses terkoneksi ke database";
-} catch(PDOException $e) {
-    echo "Koneksi gagal: " . $e->getMessage();
-}
+
