4c181c02f1a5ed93c0e436362d87d79dd772fc61
husein zidan <zidan.idros14@gmail.com>
2024-03-24 04:00:07 +0700

    commit testTransaction

---
+++ PHP/PHP Mysql Database/testTransaction.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/getConnection.php';
+
+$connection = getConnection();
+
+$connection->beginTransaction();
+
+$connection->exec("INSERT INTO tb_penjualan(ID, Name) VALUES(12,'LALAPO')");
+$connection->exec("INSERT INTO tb_penjualan(ID, Name) VALUES(13,'LALAPO')");
+$connection->exec("INSERT INTO tb_penjualan(ID, Name) VALUES(14,'LALAPO')");
+$connection->exec("INSERT INTO tb_penjualan(ID, Name) VALUES(15,'LALAPO')");
+$connection->exec("INSERT INTO tb_penjualan(ID, Name) VALUES(16,'LALAPO')");
+
+$connection->commit();
+//$connection->rollBack();//membatalkan seluruh dan id A-I tidak ke rollback
+$connection = null;
