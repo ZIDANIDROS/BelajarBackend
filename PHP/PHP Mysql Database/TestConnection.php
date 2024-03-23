59ec6e6b7f6ba08c22854c50d19b5333f5c4535c
husein zidan <zidan.idros14@gmail.com>
2024-03-23 17:46:59 +0700

    TestConnection benar

--- PHP/PHP Mysql Database/TestConnection.php
+++ PHP/PHP Mysql Database/TestConnection.php
@@ -6,12 +6,8 @@ $port       = 3306;
 $user       = 'root';
 $password   = '';
 
-try {
-    $connection = new PDO($database . ":host=" . $host . ';port=' . $port, $user, $password);
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "SUKSES";
-    return $connection;
-} catch (PDOException $e) {
-    echo $e->getMessage();
-}
+
+$connection = new PDO($database . ":host=" . $host . ';port=' . $port, $user, $password);
+echo "SUKSES";
+return $connection;
 
