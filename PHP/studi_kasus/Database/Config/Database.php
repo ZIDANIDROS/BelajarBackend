d37a31dc33b8b2c94eccd7bbd016d6c97ba5d45b
husein zidan <zidan.idros14@gmail.com>
2024-03-25 06:33:46 +0700

    Database.php

---
+++ PHP/studi_kasus/Database/Config/Database.php
@@ -0,0 +1,17 @@
+<?php 
+
+namespace Config{
+    class Database{
+        static function getConnection() : \PDO
+        {
+            $host       = 'localhost';
+            $database   = 'belajar_php_todolist';
+            $port       = 3306;
+            $user       = 'root';
+            $password   = '';
+
+            return new \PDO("mysql:host=$host;port=$port;dbname=$database", $user, $password);
+        }
+    }
+    
+}
