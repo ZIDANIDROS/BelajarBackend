b9e9add5c7d7bfcef95003169c0257dee3036a8c
husein zidan <zidan.idros14@gmail.com>
2024-03-26 12:45:38 +0700

    Todolist

--- PHP/studi_kasus/Database/Entity/Todolist.php
+++ PHP/studi_kasus/Database/Entity/Todolist.php
@@ -3,11 +3,21 @@
 namespace Entity {
     class Todolist
     {
+        private int $id;
         private string $todo;
         public function __construct(string $todo = "")
         {
             $this->todo = $todo;
         }
+
+        public function setID(int $id) : void {
+            $this->id=$id;
+        }
+
+        public function getId() : int {
+            return $this->id;
+        }
+
         public function getTodo()
         {
             return $this->todo;
