d2d65a7ed8869c5748325ce493d1a1b178891dd0
husein zidan <zidan.idros14@gmail.com>
2024-03-30 06:01:56 +0700

    w

---
+++ PHP/OOP2/inheritance/keenam.php
@@ -0,0 +1,28 @@
+<?php
+
+class Buku
+{
+    public $judul,$author;//property
+
+    public function __construct($judul,$author) {
+        $this->judul = $judul;
+        $this->author = $author;
+    }
+    //method pengembalian
+    public function lol(){
+        echo "ini judul buku {$this->judul} ,ini author {$this->author} ?".PHP_EOL;
+    }
+}
+
+class Cetak
+{
+    public function cetak(Buku $a){
+        echo "ini judul buku {$a->judul} ,authornya adalah {$a->author} ".PHP_EOL;
+    }    
+}
+
+$buku1 = new Buku("lelepali","Zidan");
+
+$infoProduk1 = new Cetak();
+$infoProduk1->cetak($buku1);
+
