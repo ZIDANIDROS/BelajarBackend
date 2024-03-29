525b8fee586127b8a7b0ff9feff71e8f191f1c59
husein zidan <zidan.idros14@gmail.com>
2024-03-30 05:23:21 +0700

    move

---
+++ PHP/OOP2/class&object/kedua.php
@@ -0,0 +1,18 @@
+<?php
+
+class kedua
+{
+    public $namaDepan,$namaBelakang;//property
+
+    function __construct($namaDepan,$namaBelakang) {
+        $this->namaDepan = $namaDepan;
+        $this->namaBelakang = $namaBelakang;
+    }
+    //method
+    public function lol(){
+        echo "apakah kamu sudah makan {$this->namaDepan} {$this->namaBelakang} ?".PHP_EOL;
+    }
+}
+
+$a = new kedua("Husein", "Zidan");
+echo $a->lol();
