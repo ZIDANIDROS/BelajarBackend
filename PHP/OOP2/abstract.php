9346c1d3f82006c4869ff772958e8750bc606e62
husein zidan <zidan.idros14@gmail.com>
2024-03-31 11:27:44 +0700

    abstract

---
+++ PHP/OOP2/abstract.php
@@ -0,0 +1,38 @@
+<?php
+
+abstract class Car {
+    public $name;
+    public function __construct($name) {
+      $this->name = $name;
+    }
+    abstract public function intro() : string;
+  }
+  
+  // Child classes
+  class Audi extends Car {
+    public function intro() : string {
+      return "Choose German quality! I'm an $this->name!"; 
+    }
+  }
+  
+  class Volvo extends Car {
+    public function intro() : string {
+      return "Proud to be Swedish! I'm a $this->name!"; 
+    }
+  }
+  
+  class Citroen extends Car {
+    public function intro() : string {
+      return "French extravagance! I'm a $this->name!"; 
+    }
+  }
+  
+  // Create objects from the child classes
+  $audi = new audi("Audi");
+  echo $audi->intro();
+  
+  $volvo = new volvo("Volvo");
+  echo $volvo->intro();
+  
+  $citroen = new Citroen("Citroen");
+  echo $citroen->intro();
