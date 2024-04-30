ea87c369d19df6b8982c5a749b0fe02952977fb5
husein zidan <zidan.idros14@gmail.com>
2024-04-30 18:32:12 +0700

    belajar PHP

---
+++ PHP/PHP_web/html.php
@@ -0,0 +1,15 @@
+<?php
+$title = "hello world from title";
+$body = "hello world from body";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title;?></title>
+</head>
+<body>
+<?php echo $body;?>
+</body>
+</html>
