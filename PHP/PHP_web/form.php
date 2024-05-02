7af7ec180e7111c80cc68a4834aa74f5078afa50
husein zidan <zidan.idros14@gmail.com>
2024-05-02 08:19:19 +0700

    form.php

---
+++ PHP/PHP_web/form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>register</h1>
+    <form action="post.php" method="post">
+        <label>
+            <input type="text" name="firstname" id="">
+        </label>
+        <br/>
+        <input type="submit" name="" id="">
+    </form>
+</body>
+</html>
