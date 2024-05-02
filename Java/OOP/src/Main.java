2b038a1b6e023525579a16ec1657d6d19afd85e0
husein zidan <zidan.idros14@gmail.com>
2024-05-02 22:26:50 +0700

    main

--- Java/OOP/src/Main.java
+++ Java/OOP/src/Main.java
@@ -1,5 +1,38 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+abstract class Hewan {
+
+    // Mendeklrasikan class dan method tipe abstract
+    protected abstract void munculSuara();
+
+}
+
+class Kucing extends Hewan {
+
+    // Menggunakan method dari kelas induk abstrak
+    @Override
+    protected void munculSuara() {
+        System.out.println("Suara Kucing: Meow...meow..meow.");
     }
+
+}
+
+class Burung extends Hewan {
+
+    // Menggunakan method dari kelas induk abstrak
+    @Override
+    protected void munculSuara() {
+        System.out.println("Suara Burung: Cit...cit..cit.");
+    }
+
+}
+
+public class Tampil {
+
+    public static void main (String[] args) {
+        Hewan kucing = new Kucing();
+        kucing.munculSuara();
+
+        Hewan burung = new Burung();
+        burung.munculSuara();
+    }
+
 }
