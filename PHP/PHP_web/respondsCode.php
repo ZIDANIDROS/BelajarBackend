9b963d3c9bffb354ccb066b9205fda1f0ee31236
husein zidan <zidan.idros14@gmail.com>
2024-05-05 14:30:20 +0700

    waiting

---
+++ PHP/PHP_web/respondsCode.php
@@ -0,0 +1,21 @@
+<?php
+if (!isset($_GET['name']) || $_GET['name'] == "") {
+    http_response_code(400);
+    echo "Name is required";
+    exit();
+}
+
+$say = "Hello". htmlspecialchars($_GET['name']);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>respondsCode.php</title>
+</head>
+<body>
+    <h1><?= $say?></h1>
+</body>
+</html>
