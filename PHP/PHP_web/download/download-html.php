09a03d335cd97f9c67d54dbc4f583aebf15e6075
husein zidan <zidan.idros14@gmail.com>
2024-05-10 04:57:00 +0700

    donlodhtml

---
+++ PHP/PHP_web/download/download-html.php
@@ -0,0 +1,15 @@
+<?php
+header('Content-Disposition: attachment; filename="download.html"')
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>download-html.php</title>
+</head>
+<body>
+    <h1>download-html</h1>
+</body>
+</html>
