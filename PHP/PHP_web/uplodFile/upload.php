e4ab1ed5f9a23e80bcfd6e4305ac711bbdeccf5f
husein zidan <zidan.idros14@gmail.com>
2024-05-10 04:07:55 +0700

    uplodFile

---
+++ PHP/PHP_web/uplodFile/upload.php
@@ -0,0 +1,53 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD']=="POST") {
+    $file_name = $_FILES['upload_file']['name'];
+    $file_type = $_FILES['upload_file']['type'];
+    $file_size = $_FILES['upload_file']['size'];
+    $file_tmp_name = $_FILES['upload_file']['tmp_name'];
+    $file_error = $_FILES['upload_file']['error'];
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>upload.php</title>
+</head>
+<body>
+    <?php if ($_SERVER['REQUEST_METHOD']=="POST") { ?>
+        <table>
+            <tr>
+                <td>File Name</td>
+                <td><?= $file_name ?></td>
+            </tr>
+            <tr>
+                <td>File Type</td>
+                <td><?= $file_type ?></td>
+            </tr>
+            <tr>
+                <td>File Size</td>
+                <td><?= $file_size ?> kb</td>
+            </tr>
+            <tr>
+                <td>File temp name</td>
+                <td><?= $file_tmp_name ?></td>
+            </tr>
+            <tr>
+                <td>File error</td>
+                <td><?= $file_error ?></td>
+            </tr>
+        </table>
+    <?php } ?>
+
+    <h1>Form Uplod</h1>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <ltafor="">
+            <input type="file" name="upload_file">
+        </ltafor=>
+        <input type="submit" value="Upload">
+    </form>
+</body>
+</html>
