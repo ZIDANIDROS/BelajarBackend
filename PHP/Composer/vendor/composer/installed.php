3956ba19075d0ccd7a30e43f08dcdeb2dee85dcb
husein zidan <zidan.idros14@gmail.com>
2024-05-20 06:34:25 +0700

    vendor

---
+++ PHP/Composer/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'huseinzidan/belajar-composer',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '82c858e50be9ae8e7efd9ff48ef63e56e7b436a4',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'huseinzidan/belajar-composer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '82c858e50be9ae8e7efd9ff48ef63e56e7b436a4',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
