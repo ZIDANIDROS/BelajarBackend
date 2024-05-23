130d49fb62e3bd77ebcde06eba5d8f329487703c
husein zidan <zidan.idros14@gmail.com>
2024-05-23 07:38:28 +0700

    jadi

--- PHP/Composer/belajarLibrary/src/Customer.php
+++ PHP/Composer/belajarLibrary/src/Customer.php
@@ -2,4 +2,12 @@
 
 namespace huseinzidan\belajar;
 
-class Customer 
+class Customer {
+    public function __construct(private string $name){
+
+    }
+
+    public function sayHello(string $name): string{
+        return "Hello $name, my name is $this->name"
+    }
+}
