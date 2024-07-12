ce8acea2dfee0659f15d6f7cbe8a798696b6cd58
husein zidan <zidan.idros14@gmail.com>
2024-07-12 12:49:36 +0700

    d

---
+++ PHP/todolistSederhana/belajarhttp/welcome.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+    <link rel="stylesheet" href="/../gaya.css">
+</head>
+<body>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Mengambil data dari array $_POST
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    
+    // Menampilkan data yang diterima
+    echo "Welcome, $name!<br>";
+    echo "Your email address is: $email";
+} else {
+    echo "Form was not submitted properly.";
+}
+?>
+
+</body>
+</html>
