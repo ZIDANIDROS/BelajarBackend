de91dc4dcb66d7df4ea50f0959764dfa0b28e1dd
husein zidan <zidan.idros14@gmail.com>
2024-07-25 20:46:09 +0700

    sa

--- PHP/univ cuy/index.php
+++ PHP/univ cuy/index.php
@@ -1,19 +1,15 @@
-<?php 
-
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>iya</title>
 </head>
 <body>
+    <?php include "layout/header.html" ?>
     <main>
         <p>selamat datang di web testing</p>
     </main>
+    <?php include "layout/footer.html" ?>
 </body>
 </html>
