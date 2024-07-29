39b43ee7814dd1453afd91ffa8fad6673adce7a3
husein zidan <zidan.idros14@gmail.com>
2024-07-30 01:49:53 +0700

    m

--- PHP/univ cuy/login.php
+++ PHP/univ cuy/login.php
@@ -1,19 +1,33 @@
+<?php
+include "service/Databest.php";
+
+if (isset($_POST['login'])) {
+    $username = $_POST["username"];
+    $pw = $_POST["pw"];
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>iya</title>
 </head>
+
 <body>
     <?php include "layout/header.html" ?>
     <a href="index.php">Home</a>
     <h3>Login</h3>
-    <form action="">
+    <form action="login.php" method="POST">
         <input type="text" placeholder="username" name="username">
-        <input type="password" placeholder="password" name="password"> 
-        <button type="submit">Login</button>
+        <input type="password" placeholder="password" name="pw">
+        <button type="submit" name="login">Login</button>
     </form>
     <?php include "layout/footer.html" ?>
 </body>
+
 </html>
