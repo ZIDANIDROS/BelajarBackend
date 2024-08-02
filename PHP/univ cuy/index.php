254bf99a261e71afd3972f95b099497a0ca59e63
husein zidan <zidan.idros14@gmail.com>
2024-08-02 08:37:04 +0700

    index

--- PHP/univ cuy/index.php
+++ PHP/univ cuy/index.php
@@ -6,16 +6,19 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>iya</title>
 </head>
-<body>
+
+<body style="background-color: black;color:aliceblue;">
     <?php include "layout/header.html" ?>
     <main>
         <p>selamat datang di web testing</p>
     </main>
     <?php include "layout/footer.html" ?>
 </body>
+
 </html>
