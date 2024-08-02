4c9e4977a9512b6122ac8d47a3cb291943e31d67
husein zidan <zidan.idros14@gmail.com>
2024-08-02 08:37:19 +0700

    signup

--- PHP/univ cuy/signup.php
+++ PHP/univ cuy/signup.php
@@ -1,7 +1,32 @@
 <?php
 
 include "service/Databest.php";
+session_start();
 
+$signup_mssage = "";
+
+if (isset($_SESSION["is_login"])) {
+    header("location: dashboard.php");
+}
+
+
+if (isset($_POST["signup"])) {
+    $username = $_POST["username"];
+    $pw = $_POST["pw"];
+
+    try {
+        $sql = "INSERT INTO users (username,password) VALUES ('$username','$pw')";
+
+        if ($db->query($sql)) {
+            $signup_mssage = "Daftar sukses";
+        } else {
+            $signup_mssage = "gagal, silahkan muncul lagi";
+        }
+    } catch (mysqli_sql_exception) {
+        $signup_mssage = "Username sudah ada";
+    }
+    $db->close();
+}
 
 ?>
 
@@ -14,14 +39,15 @@ include "service/Databest.php";
     <title>iya</title>
 </head>
 
-<body>
+<body style="background-color: black;color:aliceblue">
     <?php include "layout/header.html" ?>
     <a href="index.php">Home</a>
     <h3>DAFTAR AKUN</h3>
+    <i><?= $signup_mssage ?></i>
     <form action="signup.php" method="POST">
         <input type="text" placeholder="username" name="username">
-        <input type="password" placeholder="password" name="password">
-        <button type="submit">daftar sekarang</button>
+        <input type="password" placeholder="password" name="pw">
+        <button type="submit" name="signup">daftar sekarang</button>
     </form>
     <?php include "layout/footer.html" ?>
 </body>
