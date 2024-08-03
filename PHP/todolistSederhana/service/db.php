9f25c7e30be08c53afa11941e3963108e864c4d1
husein zidan <zidan.idros14@gmail.com>
2024-08-04 00:19:37 +0700

    db

--- PHP/todolistSederhana/service/db.php
+++ PHP/todolistSederhana/service/db.php
@@ -5,8 +5,8 @@ $username = "root";
 $password = "";
 $dbname = "test";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$db = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
 }
