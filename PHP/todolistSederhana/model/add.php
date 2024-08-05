890ffda9afc08da5d858024196d510762ecc578f
husein zidan <zidan.idros14@gmail.com>
2024-08-05 19:10:54 +0700

    add

--- PHP/todolistSederhana/model/add.php
+++ PHP/todolistSederhana/model/add.php
@@ -1,2 +1,15 @@
 <?php
 include 'service/db.php';
+
+if (isset($_POST['add'])) {
+    $list = $_POST['list'];
+
+    $sql = $db->prepare("INSERT INTO list (namalist, datetime, id_user) VALUES (?, NOW(), ?, ?)");
+    $sql->bind_param("ssi", $namalist, $id_user);
+
+    if ($sql->execute()) {
+        echo "Data berhasil ditambahkan.";
+    } else {
+        echo "Gagal menambahkan data.";
+    }
+}
