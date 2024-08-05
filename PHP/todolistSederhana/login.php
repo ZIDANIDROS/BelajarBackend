ce553b65a732ec4eae6b32d69184c9e053aaff57
husein zidan <zidan.idros14@gmail.com>
2024-08-05 19:10:46 +0700

    underline

--- PHP/todolistSederhana/login.php
+++ PHP/todolistSederhana/login.php
@@ -8,7 +8,7 @@ if (isset($_POST['login'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $sql = $db->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
+    $sql = $db->prepare("SELECT id_user, username FROM users WHERE username = ? AND password = ?");
     $sql->bind_param("ss", $username, $password);
     $sql->execute();
     $result = $sql->get_result();
@@ -16,7 +16,9 @@ if (isset($_POST['login'])) {
     if ($result->num_rows > 0) {
         $data = $result->fetch_assoc();
         $_SESSION['username'] = $data['username'];
-        header('Location: dashboard.php');
+        // $_SESSION["is_login"] = true;
+        $_SESSION['id_user'] = $user['id_user'];
+        header('location: dashboard.php');
     } else {
         $login_massage = "username dan password salah";
     }
@@ -36,7 +38,7 @@ if (isset($_POST['login'])) {
 
 <body style="background-color: black;color:aliceblue;">
     <h1>SILAHKAN LOGIN</h1>
-
+    <a href="index.php">Home</a><br /><br />
     <?= $login_massage ?>
     <form action="login.php" method="POST">
         <input type="text" name="username" placeholder="username">
