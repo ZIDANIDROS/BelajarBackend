eb5399402a6c7b127eca45b25edee75bc49cac41
husein zidan <zidan.idros14@gmail.com>
2024-08-05 19:10:40 +0700

    underline

--- PHP/todolistSederhana/dashboard.php
+++ PHP/todolistSederhana/dashboard.php
@@ -1,7 +1,13 @@
 <?php
-
+include 'service/db.php';
 session_start();
+include 'model/menampilkandata.php';
+include 'model/logout.php';
 
+$id_user = $_SESSION['id_user'];
+$name = $_SESSION['username'];
+
+$db->close();
 ?>
 
 <!DOCTYPE html>
@@ -14,7 +20,27 @@ session_start();
 </head>
 
 <body style="background-color: black;color:aliceblue;">
-    <h1>Selamat Datang <?= $_SESSION['username'] ?></h1>
+    <h1>Selamat Datang di Dashboard, <?= $name . " id: " . $id_user; ?>
+    </h1>
+    <br>
+    <form action="dashboard.php" method="POST">
+        <ul>
+
+            <li>
+                <p><?php $list['namalist'] ?> - <?php $list['datetime'] ?> </p>
+                <button type="submit" name="update">update</button>
+                <button type="submit" name="delet">delet</button>
+            </li>
+
+        </ul>
+
+        <input type="text" name="namalist" placeholder="Nama List" required>
+        <button type="submit" name="add">Tambah</button>
+        <br />
+        <br />
+        <button type="submit" name="logout">LOGOUT</button>
+    </form>
+
 </body>
 
 </html>
